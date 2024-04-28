84f655523830174b121d036b3dff4c5ca236d0a2
Elena Kochegarova <elenakoch122@ya.ru>
2024-04-28 12:27:21 +0700

    refactored

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const upButton = document.querySelector('up-button');
-upButton.addEventListener('click', () => {
+const popup = document.querySelector('.popup');
 
+const feedback = document.querySelector('#feedback');
+feedback.addEventListener('click', (e) => {
+  e.preventDefault();
+  popup.classList.remove('popup_hidden');
 });
+
+const popupCloseBtn = document.querySelector('.popup__closeBtn');
+popupCloseBtn.addEventListener('click', () => popup.classList.add('popup_hidden'));
