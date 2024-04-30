baaa25d730f7ac294524081e3cc43e65ef6d9ea8
Elena Kochegarova <elenakoch122@ya.ru>
2024-04-30 22:28:03 +0700

    added opening/closing burger menu

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const popup = document.querySelector('.popup');
-
 const feedback = document.querySelector('#feedback');
+
 feedback.addEventListener('click', (e) => {
   e.preventDefault();
   popup.classList.remove('popup_hidden');
@@ -9,5 +9,17 @@ feedback.addEventListener('click', (e) => {
 const popupCloseBtn = document.querySelector('.popup__closeBtn');
 popupCloseBtn.addEventListener('click', () => popup.classList.add('popup_hidden'));
 
+const menu = document.querySelector('.menu');
 const burgerMenu = document.querySelector('.button_menu');
-burgerMenu.addEventListener('click', () => popup.classList.add('popup_hidden'));
+const heading = document.querySelector('.heading');
+
+burgerMenu.addEventListener('click', () => {
+  menu.classList.toggle('menu_visible');
+
+  menu.classList.contains('menu_visible') ? heading.style.zIndex = '0' : heading.style.zIndex = '3';
+
+  const nav = document.querySelector('.menu_visible');
+  nav.addEventListener('click', () => menu.classList.toggle('menu_visible'));
+});
+
+// при повторном нажатии на ссылку в бургер меню nav не скрывается
