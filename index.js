13726fb6221f267a5a6bbc412bb0ef8e2f579072
Elena Kochegarova <elenakoch122@ya.ru>
2024-05-01 20:22:20 +0700

    fixed - when you click on the link in the burger menu again, the menu does not hide

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ feedback.addEventListener('click', (e) => {
 const popupCloseBtn = document.querySelector('.popup__closeBtn');
 popupCloseBtn.addEventListener('click', () => popup.classList.add('popup_hidden'));
 
-const menu = document.querySelector('.menu');
 const burgerMenu = document.querySelector('.button_menu');
 const heading = document.querySelector('.heading');
 
 burgerMenu.addEventListener('click', () => {
   menu.classList.toggle('menu_visible');
-
   menu.classList.contains('menu_visible') ? heading.style.zIndex = '0' : heading.style.zIndex = '3';
-
-  const nav = document.querySelector('.menu_visible');
-  nav.addEventListener('click', () => menu.classList.toggle('menu_visible'));
 });
 
-// при повторном нажатии на ссылку в бургер меню nav не скрывается
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+  if (menu.classList.contains('menu_visible')) {
+    menu.classList.remove('menu_visible');
+    heading.style.zIndex = '3';
+  }
+});
